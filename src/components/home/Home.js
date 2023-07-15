94842a93f3a01508b5968dad456e16b74cedfa72
HongJeHyeop <jhh951217@naver.com>
2023-07-15 16:23:13 +0900

    Fix : update login check (230715)

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,8 +3,20 @@ import Header from "../Header";
 import HomeSideBar from "./HomeSideBar";
 import {Route, Routes} from "react-router-dom";
 import HomeMain from "./HomeMain";
+import {useEffect} from "react";
+import axios from "axios";
 
 function Home() {
+    useEffect( () => {
+        axios.get('/users/loginCheck')
+            .then(res => {
+                console.log(res.data)
+                if(!res.data === 'login'){
+                    window.location.href = '/';
+                }
+            })
+            .catch()
+    }, []);
     return(
         <div id="home">
             <HomeSideBar/>
--- src/components/home/HomeMain.js
+++ src/components/home/HomeMain.js
@@ -1,9 +1,22 @@
 import '../../css/home/homeMain.css';
+import axios from "axios";
 
 export default function HomeMain() {
-    return(
+    const logout = async () => {
+        await axios.post('/users/logout')
+            .then(res => {
+                console.log(res)
+                window.location.href='/'
+            })
+            .catch(err => console.log(err))
+
+    }
+
+
+    return (
         <div id="home-main">
             <h1>홈화면</h1>
+            <button onClick={logout}>logout</button>
         </div>
     )
 }
